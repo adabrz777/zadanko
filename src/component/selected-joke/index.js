160a39705af47a9a42e3f5180d016aa8f8eed617
adabrz777 <adabrz777@gmail.com>
2020-10-24 18:21:38 +0200

    Add time to new joke

--- src/component/selected-joke/index.js
+++ src/component/selected-joke/index.js
@@ -1,34 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import Categories from './categories.js';
 
+import { Button } from 'antd';
+
+import './index.css';
+
 const SelectedJoke = () => {
-    const myRef = React.createRef();
+	const myRef = React.createRef();
 
 	const JOKE_URL = 'https://api.chucknorris.io/jokes/random?category=';
 
 	const [ selectedCategory, setSelectedCategory ] = useState('animal');
 	const [ joke, setJoke ] = useState('');
-
+	const [ other, setOther ] = useState(0);
 
 	useEffect(
 		() => {
-            fetch(`${JOKE_URL}${selectedCategory}`)
-            .then(r => r.json())
-            .then(r => r.value)
-            .then((r) => setJoke(r))
+			fetch(`${JOKE_URL}${selectedCategory}`).then((r) => r.json()).then((r) => r.value).then((r) => setJoke(r));
 		},
-		[ selectedCategory ]
+		[ selectedCategory, other ]
 	);
 
-    
 	return (
-        <div>
-            {joke}<br/>
-            <select onChange={()=>{setSelectedCategory(myRef.current.value)}} ref={myRef}>
-                Category: <Categories />
-            </select>
-        </div>
-    );
+		<div className="SelectedJoke">
+			Your joke sir: {joke}
+			<br />
+			<div className='GUI'>
+				Category: <select
+					onChange={() => {
+						setSelectedCategory(myRef.current.value);
+					}}
+					ref={myRef}
+				>
+				<Categories />
+				</select>
+				<Button
+					type="primary"
+					onClick={() => {
+						setOther(other + 1);
+					}}
+				>
+					OTHER
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default SelectedJoke;
